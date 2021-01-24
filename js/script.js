cde993cb0e1f62f2a1a4882f0759e81d85db7d03
Roman <roman.kukla@mail.ru>
2021-01-24 13:15:00 +0300

    films and rating add to the object

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
 "use strict";
 
-let number = 5;
-const leftBorderWidth = 1;
+const numberOfFilms = +prompt('Сколько фильмом Вы посмотрели?', '');
 
-number = 10;
-console.log(leftBorderWidth);
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const film1 = prompt('Один из последних просмотренных фильмов?', ''),
+      rating1 = prompt('На сколько вы оцените этот фильм?', ''),
+      film2 = prompt('Один из последних просмотренных фильмов?', ''),
+      rating2 = prompt('На сколько вы оцените этот фильм?', '');
+
+personalMovieDB.movies[film1] = rating1;
+personalMovieDB.movies[film2] = rating2;
+
+console.log(personalMovieDB)
